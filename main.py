3619fcfef34a6b2ac40108b3b8fe3fb96613af80
Shakhan <abdiev@globalpas.com>
2024-03-26 23:03:38 +0500

    fourth lab implement

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("HELLO world");
+# 8 Проверка Палиндрома
+# - Реализовать рекурсивную функцию для проверки, является ли строка
+# палиндромом.
+
+def lab_three_palindromic(string = ""):
+    if len(string) <= 1:
+        return True
+    if string[0] == string[-1]:
+        return lab_three_palindromic(string[1:-1])
+    else:
+        return False
+
+a = input()
+print(lab_three_palindromic(a))
+
+
