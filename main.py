80fd9d208604685f42e1c457f43f15d07b971146
Shakhan <abdiev@globalpas.com>
2024-03-27 01:27:29 +0500

    beautify

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from functools import reduce
 # - Преобразовать список строк в заглавный регистр, отфильтровать строки длиннее 5
 # символов и объединить их в одну строку.
 
-def lab_one_format_string(array=[]):
-    upper_cased_array = map(lambda x: x.upper(), array);
-    filtered_array_length_more_five = filter(lambda x: len(x) > 5, upper_cased_array);
-    return reduce(lambda x, y: x + y, filtered_array_length_more_five);
+def lab_one_format_string(array = []):
+    upper_cased_array = map(lambda x: x.upper(), array)
+    filtered_array_length_more_five = filter(lambda x: len(x) > 5, upper_cased_array)
+    return reduce(lambda x, y: x + y, filtered_array_length_more_five)
 
 
-print(lab_one_format_string(["assssssssssss", "2342342dffvdf", "efjeibgierugieurgiergiuer", "123"]));
+print(lab_one_format_string(["assssssssssss", "2342342dffvdf", "efjeibgierugieurgiergiuer", "123"]))
 
 
