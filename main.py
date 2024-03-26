7e97cc2f73a1c0153232f8fa2fbb5bfa35ab22e4
Shakhan <abdiev@globalpas.com>
2024-03-26 23:00:08 +0500

    second lab implement

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("HELLO world");
+
+
+# 3 Генератор Функций
+# - Создать функцию высшего порядка, которая возвращает другую функцию для
+# возведения числа в указанную степень.
+
+def lab_two_high_order_function_fabric(x):
+    def fabric_function(y):
+        return x ** y;
+
+    return fabric_function;
+
+
+# print(lab_two_high_order_function_fabric(5)(3));
+
